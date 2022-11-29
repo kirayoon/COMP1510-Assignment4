33ea446cbdcb09314fe9c87c89370de256ee1405
Joseph Chun <83483639+Mrrm2@users.noreply.github.com>
2022-11-28 17:57:37 -0800

    Add default function

--- levels/level_1.py
+++ levels/level_1.py
@@ -14,4 +14,24 @@ def check_event(board: dict, player_dict: dict):
     event_func(player_dict)
 
 
+def default(player_dict: dict):
+    choice = random.randint(1, 2)
+    if choice == 1:
+        print('''
+        You're in the middle of a river.
+        There's water rushing all around you. 
+        The rocks are slippery! Don't fall!
+        ''')
+    else:
+        print('''
+        Water rushes all around you.
+        You see fish jumping in the distance.
+        Your tummy rumbles.
+        ''')
+    if 'water' in player_dict['inventory']:
+        player_dict['inventory']['water'] += 1
+    else:
+        player_dict['inventory']['water'] = 1
+
+
 
