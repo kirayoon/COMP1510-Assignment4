2853e3354bf1a2ed22d322fd898dfaae30175d4a
kirayoon <113317993+kirayoon@users.noreply.github.com>
2022-12-02 18:43:13 -0800

    Update scraps function in level_2.py

--- levels/level_2.py
+++ levels/level_2.py
@@ -22,38 +22,52 @@ def scraps(player_dict: dict):
         ''')
         choice = input("Eat the rat? (y/n)")
         if choice == 'y':
-            print('yummy.. You gained 1 HP!')
-            player_dict['hp'] += 1
+            print('yummy.. You gained 5 HP & 10 XP!')
+            player_dict['hp'] += 5
+            player_dict['xp'] += 10
         elif choice == 'n':
             pass
         else:
             print('Invalid input. Enter y or n')
+
     elif rand == 2:
         print('''
         WoooaaaaAAAAHHH!
         You slipped on a banana peel and fell on your face. 
         You have a nosebleed. boohoo :(
     
-        You lost 1 hp
+        You lost 1 HP & gained 10 XP
         ''')
-        player_dict['hp'] -= 1
+        player_dict['hp'] -= 5
+        player_dict['xp'] += 10
+
     elif rand == 3:
         print('''
         You found a jar of honey!
         ''')
-        choice = input("Eat the honey? (y/n)")
+        choice = input("Steal the honey? (y/n)")
         if choice == 'y':
-            print('yummy.. You gained 1 HP!')
-            player_dict['hp'] += 1
+            if 'honey' in player_dict['inventory']:
+                player_dict['inventory']['honey'] += 1
+            else:
+                player_dict['inventory']['honey'] = 1
+            print('1 honey has been added to your inventory')
         elif choice == 'n':
-            pass
+            print('''
+            You left the honey alone.
+            What an ethical choice!
+            
+            You gained 10 XP!
+            ''')
+            player_dict['xp'] += 10
         else:
             print('Invalid input. Enter y or n')
+
     else:
         print('''
         OUCH! 
         You stepped on a nail!
         
-        You lost 1 hp
+        You lost 3 hp
         ''')
-        player_dict['hp'] -= 1
+        player_dict['hp'] -= 3
