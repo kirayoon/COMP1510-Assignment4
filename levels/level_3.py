da852245e5c1c1fb7879650791ae6a2d5d8685f0
kirayoon <113317993+kirayoon@users.noreply.github.com>
2022-12-02 19:58:35 -0800

    Add level_3 mushroom function

--- levels/level_3.py
+++ levels/level_3.py
@@ -4,11 +4,35 @@ import time
 
 
 def default(player_dict: dict):
-    pass
+    print('''
+    nothing here...
+    ''')
 
 
 def mushroom(player_dict: dict):
-    pass
+    print('''
+    You found a mushroom.''')
+    choice = input('    Eat mushroom? (y/n)')
+    if choice == 'y':
+        rand = random.randint(1, 2)
+        if rand == 1:
+            print('''
+    You ate the mushroom. Delicious!
+    You gained 5 HP.
+    ''')
+            player_dict['hp'] += 5
+        else:
+            print('''
+    You ate a POISONOUS mushroom. YUCK!
+    You lose 5 HP.
+    ''')
+            player_dict['hp'] -= 5
+
+    elif choice == 'n':
+        print('''
+    You left the mushroom alone.''')
+    else:
+        print('Invalid input. Enter y or n')
 
 
 def berry(player_dict: dict):
@@ -21,5 +45,3 @@ def nut(player_dict: dict):
 
 def deer(player_dict: dict):
     pass
-
-
