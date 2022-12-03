07e76a3b8b5e5a988f4b5ef30b4d50f99f9b8f7d
kirayoon <113317993+kirayoon@users.noreply.github.com>
2022-12-02 19:48:45 -0800

    Set up for level 3

---
+++ levels/level_3.py
@@ -0,0 +1,25 @@
+import random
+import json
+import time
+
+
+def default(player_dict: dict):
+    pass
+
+
+def mushroom(player_dict: dict):
+    pass
+
+
+def berry(player_dict: dict):
+    pass
+
+
+def nut(player_dict: dict):
+    pass
+
+
+def deer(player_dict: dict):
+    pass
+
+
